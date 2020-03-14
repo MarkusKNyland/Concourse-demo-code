86146cf044b909bc2bd1cbc82bba092896b2314a
Markus K Nyland <markus.nyland@evry.com>
2020-03-14 21:58:04 +0100

    Init with a small java Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -22,12 +22,18 @@ class Calculator{
         return sum;
     }
 
+    private void toD(){
+        int[][] a ={{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println("" + a[2][2]);
+    }
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.renter(500000, 2300));
-        System.out.println("#######");
-        System.out.println(calculator.renter(500000, 3300));
-        System.out.println("#######");
-        System.out.println(calculator.renter(500000, 7900));
+//        System.out.println(calculator.renter(500000, 2300));
+//        System.out.println("#######");
+//        System.out.println(calculator.renter(500000, 3300));
+//        System.out.println("#######");
+//        System.out.println(calculator.renter(500000, 7900));
+        calculator.toD();
     }
 }
